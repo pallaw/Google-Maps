c8c2d51b94fdb38ca6a200e75d0b688efb66734c
Pallaw Pathak <pallaw.pathak@gmail.com>
2020-06-28 19:45:44 +0530

    current location using fused location api

--- app/src/main/java/com/pallaw/mapspractice/MapsActivity.kt
+++ app/src/main/java/com/pallaw/mapspractice/MapsActivity.kt
@@ -1,14 +1,17 @@
 package com.pallaw.mapspractice
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -20,7 +23,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var mLocationPermissionGranted: Boolean = false
     private val LOCATION_PERMISSION_REQUEST_CODE: Int = 678
-    private var permissionGranted: Boolean = false
     private val ERROR_DIALOG_REQUEST: Int = 123
     private lateinit var mMap: GoogleMap
     private val TAG = "MapsActivity"
@@ -33,7 +35,36 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         if (isServiceAvailable()) {
             getLocationPermission()
         } else {
+            Toast.makeText(
+                this,
+                "Your device doesn't have google play service, so you can not use google maps",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
 
+    @SuppressLint("MissingPermission")
+    private fun getDeviceLocation() {
+        mMap.isMyLocationEnabled = true
+        mMap.uiSettings.isMyLocationButtonEnabled = false
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (mLocationPermissionGranted) {
+            fusedLocationProviderClient.lastLocation.addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d(TAG, "getDeviceLocation: current location fetched ")
+                    val result = it.result
+                    result?.let {
+                        moveCameraTo(LatLng(it.latitude, it.longitude))
+                    }
+                } else
+                    Log.d(TAG, "getDeviceLocation: current location is not fetched ")
+            }
+        }
+    }
+
+    private fun moveCameraTo(result: LatLng) {
+        result.let {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(it, 10.5f))
         }
     }
 
@@ -63,6 +94,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     LOCATION_PERMISSION_REQUEST_CODE
                 )
             } else {
+                mLocationPermissionGranted = true
                 initMap()
             }
         }
@@ -114,10 +146,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        getDeviceLocation()
     }
 }
